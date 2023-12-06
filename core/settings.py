013363ae779f322029641ae3d324f110ed69ff13
Prod <pyrod7@gmail.com>
2023-12-06 15:05:12 -0600

    Se agrego el AUTH_USER_MODEL en settings

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# CUSTOM USER
+AUTH_USER_MODEL = "users.CustomUser"
+
+
 # ACTIVATE EMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
