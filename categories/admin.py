0a96011f69ba98b10b12c18f36d4b5f8e94d290c
Prod <pyrod7@gmail.com>
2023-12-06 17:43:03 -0600

    Se personalizo el admin de categories

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Categories
 # Register your models here.
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title", )}
+    list_display = ("id", "title", "is_active")
+    list_display_links = ("id", "title")
+    list_editable = ("is_active",)
+    list_filter = ("is_active",)
+    list_per_page = 20
+    search_fields = ("id", "title")
