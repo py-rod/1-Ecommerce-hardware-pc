c33d33b64440f84051c12de448d10c55da2c066b
Prod <pyrod7@gmail.com>
2023-12-06 17:33:51 -0600

    Se creo el modelo de categorias

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
+import os
+from django.urls import reverse
 # Create your models here.
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length=200, blank=False, unique=True)
+    slug = models.SlugField(unique=True, blank=False)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return reverse("products_by_categories", args=[self.slug])
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        db_table = "Categories"
