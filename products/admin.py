33feb84416002df8a82faadd7f0bd9bda1b9ef75
Prod <pyrod7@gmail.com>
2023-12-06 18:44:57 -0600

    Se creo el modelo de products, y se agrego en el admin tambien

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("product_name",)}
+    list_display = ("id", "product_name", "series", "is_active")
+    list_display_links = ("id", "product_name")
+    list_filter = ("series", "is_active")
+    list_editable = ("series", "is_active")
+    list_per_page = 20
+    search_fields = ("id", "product_name", "series")
--- products/models.py
+++ products/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
+import os
+from tinymce.models import HTMLField
+from categories.models import Categories
+from django.urls import reverse
 # Create your models here.
+
+
+class Product(models.Model):
+    def image_upload_to(self, instance):
+        if instance:
+            return os.path.join("Products_images", slugify(self.product_name), instance)
+        return None
+
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True, blank=False)
+    description = HTMLField(default="", blank=False)
+    brand = models.CharField(max_length=200, default="",
+                             unique=True, blank=False)
+    price = models.IntegerField(default=0, blank=False)
+    offer_price = models.IntegerField(default=0, blank=True)
+    old_price = models.IntegerField(default=0, blank=True)
+    percentage = models.CharField(max_length=100, default="", blank=True)
+    stock = models.IntegerField(default=0, blank=False)
+    image = models.ImageField(
+        default="./default/placeholder.webp", upload_to=image_upload_to, max_length=5000)
+    series = models.ForeignKey(Categories, default="", blank=False,
+                               help_text="Select the category to which this product belongs", on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def save(self, *args, **kwargs):
+        if self.offer_price > 0:
+            actual_price = self.price
+            new_price = (self.price - self.offer_price)
+            self.price = new_price
+            self.old_price = actual_price
+            percentage = (self.offer_price / self.price) * 100
+            self.percentage = f"{percentage:.0f}%"
+            super().save(*args, *kwargs)
+        else:
+            self.old_price = 0
+            self.percentage = ""
+            super().save(*args, *kwargs)
+
+    def get_url(self):
+        return reverse("product_detail", args=[self.series.slug, self.slug])
+
+    def __str__(self):
+        return self.product_name
