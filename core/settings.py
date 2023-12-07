a3418a36d6a4e4a675f2d10c82e95cc0ad971fdb
Prod <pyrod7@gmail.com>
2023-12-06 18:46:44 -0600

    Se agrego la configuracion de tinymce en settigs de core

--- core/settings.py
+++ core/settings.py
@@ -171,3 +171,87 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 PASSWORD_RESET_TIMEOUT = 1440
+
+
+# CONFIG TINYMCE
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
+TINYMCE_DEFAULT_CONFIG = {
+    "skin": 'oxide-dark',
+    "custom_undo_redo_levels": 100,
+    "selector": "textarea",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "link image preview codesample contextmenu table code lists fullscreen textcolor emoticons",
+    "toolbar1": "undo redo | backcolor forecolor casechange permanentpen formatpainter removeformat formatselect fontselect fontsizeselect",
+    "toolbar2": "bold italic underline blockquote | alignleft aligncenter alignright alignjustify "
+    "| bullist numlist | outdent indent | table | link image | codesample | preview code | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry",
+    "contextmenu": "formats | link image",
+    "block_formats": "Paragraph=p; Header 1=h1; Header 2=h2",
+    "fontsize_formats": "8pt 10pt 12pt 14pt 16pt 18pt",
+    "file_picker_types": 'image',
+    "image_class_list": [{"title": "Fluid", "value": "img-fluid", "style": {}}],
+    "width": "100%",
+    "height": "600px",
+    "image_caption": True,
+    "content_css": [
+        'https://cdn.jsdelivr.net/npm/prismjs@1.24.1/themes/prism.css'
+    ],
+
+    "setup": """function (editor) {
+    editor.on('init', function () {
+      editor.contentDocument.body.classList.add('language-markup'); 
+    });
+    }""",
+    "file_picker_callback": """
+        function (cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+
+            input.onchange = function () {
+                var file = this.files[0];
+
+                var reader = new FileReader();
+                reader.onload = function () {
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+
+                    cb(blobInfo.blobUri(), { title: file.name });
+                };
+                reader.readAsDataURL(file);
+            };
+            input.click();
+        }
+    """,
+    "content_style": "body { font-family: Arial; background: rgb(32, 45, 83); color: white; font-size: 12pt}",
+    "codesample_languages": [
+        {"text": "HTML/XML", "value": "markup"},
+        {"text": "JavaScript", "value": "javascript"},
+        {"text": "CSS", "value": "css"},
+        {"text": "PHP", "value": "php"},
+        {"text": "Ruby", "value": "ruby"},
+        {"text": "Python", "value": "python"},
+        {"text": "Java", "value": "java"},
+        {"text": "C", "value": "c"},
+        {"text": "C#", "value": "csharp"},
+        {"text": "C++", "value": "cpp"},
+        {"text": "Bash/Shell", "value": "bash"},
+        {"text": "CoffeeScript", "value": "coffeescript"},
+        {"text": "Diff", "value": "diff"},
+        {"text": "Erlang", "value": "erlang"},
+        {"text": "Groovy", "value": "groovy"},
+        {"text": "JSON", "value": "json"},
+        {"text": "Less", "value": "less"},
+        {"text": "Makefile", "value": "makefile"},
+        {"text": "Markdown", "value": "markdown"},
+        {"text": "Objective-C", "value": "objectivec"},
+        {"text": "R", "value": "r"},
+        {"text": "Sass", "value": "sass"},
+        {"text": "SCSS", "value": "scss"},
+        {"text": "SQL", "value": "sql"},
+        {"text": "TypeScript", "value": "typescript"},
+        {"text": "YAML", "value": "yaml"}
+    ],
+}
