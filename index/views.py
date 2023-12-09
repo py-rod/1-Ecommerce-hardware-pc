b98aed406a849d2e7fded5b0521d2e38b16ea605
Prod <pyrod7@gmail.com>
2023-12-09 01:23:39 -0600

    Se agrego la seccion 4 de solo productos con descuentos

--- index/views.py
+++ index/views.py
@@ -3,6 +3,7 @@ from .models import Carousel
 from django.utils import timezone
 from datetime import timedelta
 from products.models import Product
+import random
 # Create your views here.
 
 
@@ -14,7 +15,12 @@ def index(request):
     recent_products = Product.objects.filter(
         series__is_active=True, is_active=True, stock__gt=0, created__gte=date_now)
 
+    discount_products = Product.objects.filter(
+        series__is_active=True, is_active=True, stock__gt=0, offer_price__gt=0)
+    print(discount_products)
+
     return render(request, "index.html", {
         "images_carousel": images_carousel,
-        "recent_products": recent_products
+        "recent_products": recent_products,
+        "discount_products": discount_products
     })
