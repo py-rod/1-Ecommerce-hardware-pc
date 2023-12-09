f45541bcb20f06c5ceb6b073731e4987940aad46
Prod <pyrod7@gmail.com>
2023-12-08 19:03:41 -0600

    Se le dio estilo a la parte de productos recientes en el index

--- index/views.py
+++ index/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Carousel
 from django.utils import timezone
-from datetime import datetime
+from datetime import timedelta
+from products.models import Product
 # Create your views here.
 
 
@@ -9,7 +10,11 @@ def index(request):
     images_carousel = Carousel.objects.filter(
         series__is_active=True, series__stock__gt=0)
     images_carousel = list(enumerate(images_carousel))
+    date_now = timezone.now() - timedelta(days=30)
+    recent_products = Product.objects.filter(
+        series__is_active=True, is_active=True, stock__gt=0, created__gte=date_now)
 
     return render(request, "index.html", {
-        "images_carousel": images_carousel
+        "images_carousel": images_carousel,
+        "recent_products": recent_products
     })
